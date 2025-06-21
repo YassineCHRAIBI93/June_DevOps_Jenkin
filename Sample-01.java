5f913793cb735ddfa8b9d6d20618013d00b88d0d
yassineCHRAIBI93 <yassine-556@hotmail.fr>
2025-06-21 05:16:59 +0200

    InitialCommit

---
+++ Sample-01.java
@@ -0,0 +1,8 @@
+class Sample01{
+    public void display(){
+        System.out.println("Welcome");
+    }
+    public static void main(String[] args) {
+        System.out.println("Welcome");
+    }
+}
---
+++ Sample-03.java
@@ -0,0 +1,8 @@
+class Sample03{
+    public void display(){
+        System.out.println("Welcome");
+    }
+    public static void main(String[] args) {
+        System.out.println("Welcome");
+    }
+}
