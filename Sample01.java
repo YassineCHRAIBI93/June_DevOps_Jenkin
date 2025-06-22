3b06589391f48c2b1a6c09f475e0211a0dcadef7
yassineCHRAIBI93 <yassine-556@hotmail.fr>
2025-06-22 05:04:20 +0200

    FourthCommit

---
+++ Sample01.java
@@ -0,0 +1,9 @@
+class Sample01{
+    public void display(){
+        System.out.println("Welcome");
+    }
+     public static void main(String[] args) {
+        Sample01 sample = new Sample01();
+        sample.display();
+    }
+}
---
+++ Sample02.java
@@ -0,0 +1,8 @@
+class Sample02{
+    public void display(){
+        System.out.println("Welcome");
+    }
+    public static void main(String[] args) {
+        System.out.println("Welcome");
+    }
+}
